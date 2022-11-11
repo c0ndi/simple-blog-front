53380d6e106a1356622f71d315be5d242440c732
c0ndi <kowalik0konrad@gmail.com>
2022-11-11 02:44:55 +0100

    [Setup] Adding .env.example

--- lib/getSinglePost.ts
+++ lib/getSinglePost.ts
@@ -1,5 +1,7 @@
+import {strapiURL} from "../config/config";
+
 export async function getSinglePost(slug: string) {
-   const res = await fetch(`http://localhost:1337/api/blog-posts?filters[slug][$eq]=${slug}`)
+   const res = await fetch(`${strapiURL}/api/blog-posts?filters[slug][$eq]=${slug}`)
    const data = res.json();
 
    return data;
